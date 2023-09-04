56959e15abdfb63dafa48f24e506a591531c6122
Ubuntu <habeel@uservm.qwksj4boizsu3lu5ypgplwstqh.bx.internal.cloudapp.net>
2023-09-04 15:36:50 +0000

    first o=program hello world

---
+++ src/main/java/hellohabeelmavenpackage/hellohabeelmavenclass.java
@@ -0,0 +1,14 @@
+package hellohabeelmavenpackage;
+
+public class hellohabeelmavenclass {
+
+	public hellohabeelmavenclass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+	 System.out.print("hello habeel kunjikoylu is your love");
+
+	}
+
+}
